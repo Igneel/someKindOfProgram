4018173b11b5596b1e9ef612ad7229b2d7917e5c
Igneel <dragon.ignil@gmail.com>
2016-06-27 05:12:56 +0600

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,28 @@
+import socket
+
+def netcat(hostname, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    while 1:
+        data = s.recv(1024)
+        if data == "":
+            break
+        print "Received:", repr(data)
+    print "Connection closed."
+    s.close()
+    
+import socket
+
+def netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, int(port)))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096)
+        if not data:
+            break
+        print(repr(data))
+    s.close()
