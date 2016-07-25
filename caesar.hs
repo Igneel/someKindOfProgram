6b20512573e114c6c582050c8659aa2eede7e3ea
Igneel <dragon.ignil@gmail.com>
2016-07-25 17:04:18 +0400

    Update caesar.hs

--- caesar.hs
+++ caesar.hs
@@ -1,8 +1,8 @@
 s c | c=='z' = 'a'
-| otherwise = succ c
+  | otherwise = succ c
 p c | c=='a' = 'z'
-| otherwise = pred c
+  | otherwise = pred c
 text = "vbqwyivxgshofjeiycfbu"
 f 0 text= [map (s) text]
 f counter text= [newtext] ++ (f (counter-1) newtext)
-where newtext=map (s) text
+  where newtext=map (s) text
