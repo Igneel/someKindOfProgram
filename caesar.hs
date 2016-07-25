c9fe503d44bbe5b0e952a6ddca65760b65049c45
Igneel <dragon.ignil@gmail.com>
2016-07-25 17:01:19 +0400

    Create caesar.hs

---
+++ caesar.hs
@@ -0,0 +1,8 @@
+s c | c=='z' = 'a'
+| otherwise = succ c
+p c | c=='a' = 'z'
+| otherwise = pred c
+text = "vbqwyivxgshofjeiycfbu"
+f 0 text= [map (s) text]
+f counter text= [newtext] ++ (f (counter-1) newtext)
+where newtext=map (s) text
